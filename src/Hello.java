aeffe9b025906f4bfca9c324031cf772c92f028b
gongzu <885120431@qq.com>
2021-11-15 21:05:26 +0800

    change hello.java

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args){
-        System.out.println("console: hello world");
+        System.out.println("console: hello world   hello github");
     }
 }
